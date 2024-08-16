8c47e0c62e422e8c0309a830c80e1b81ec6753b2
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2024-08-16 05:35:08 -0300

    er

--- CursoJS/aula011/exe008.js
+++ CursoJS/aula011/exe008.js
@@ -1,1 +1,6 @@
-console.log('o exercicio funcionou corretamente')
+var velocidade = 51
+console.log(`A velocidade do seu carrro foi ${velocidade} Km/h`)
+    if (velocidade > 50) { // Condição Simples
+        console.log('Voce ultrapassou o limite de velocidade. MULTADO!')
+    }
+console.log('Dririja sempre usando o cinto de segurança')
