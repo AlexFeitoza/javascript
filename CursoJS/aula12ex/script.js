152b7ce993e9fe4deb1330d9889ab2185b38d79f
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2024-08-18 16:33:42 -0300

    exercicio 1 resolvido

--- CursoJS/aula12ex/script.js
+++ CursoJS/aula12ex/script.js
@@ -1,1 +1,21 @@
-window.alert('Deus é Fiel')
+function carregar() {
+var msg = window.document.getElementById('text')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+//var hora = data.getHours()
+var hora = 1
+msg.innerHTML = `Agora são ${hora} horas!`
+    if(hora >= 0 && hora < 12) {
+        // BOM DIA
+        img.src = 'imagens/lindo-amanhecer-na-praia-sob-um-ceu-azul_181624-26939.avif'
+        document.body.style.background = '#00BFFF'
+    }else if(hora >= 12 && hora <= 18)  {
+        //BOA TARDE
+        img.src = 'imagens/tarde.jpg'
+        document.body.style.background = '#F4A460'
+    }  else {
+        // BOA NOITE
+        img.src = 'imagens/noite.jpg'
+        document.body.style.background = '#696969'
+    }
+}
