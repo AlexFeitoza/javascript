036f1ddc0bb129402c095b69986e382888be8d70
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2024-08-21 06:39:38 -0300

    finish

--- CursoJS/aula12ex/exe15/script.js
+++ CursoJS/aula12ex/exe15/script.js
@@ -8,6 +8,43 @@ function verificar() {
    } else {
         var fsex = window.document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade} anos`
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+          gênero = 'Homem'
+               if (idade >= 0 && idade < 12) {
+                    //Criança
+                    img.setAttribute('src', 'boy.jpg')
+               } else if (idade <= 24) {
+                    //Jovem
+                    
+                    img.setAttribute('src', 'young-man.jpg')
+               } else if(idade < 60) {
+                    //Adulto
+                    img.setAttribute('src', 'homem-adulto.jpg')
+               } else {
+                    //Idoso
+                    img.setAttribute('src', 'old-man.jpg')
+               }
+        } else if (fsex[1].checked) {
+          gênero = 'Mulher'
+               if (idade >= 0 && idade < 12) {
+                    //Criança
+                    img.setAttribute('src', 'girl.jpg')
+               } else if (idade < 30) {
+                    //Jovem
+                    img.setAttribute('src', 'woman.jpg')
+               } else if (idade < 60) {
+                    //Adulto
+                    img.setAttribute('src', 'mulher-adulta.jpg')
+               } else {
+                    //Idoso
+                    img.setAttribute('src', 'old-woman.jpg')
+               }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com idade de ${idade} anos.`
+        res.appendChild(img)
     }
 }
