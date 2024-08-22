6dad7d78052b8077f4a8d215f9bce45fb7b61003
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2024-08-22 08:41:21 -0300

    Create ambiente.js

---
+++ CursoJS/aula013/ambiente.js
@@ -0,0 +1,14 @@
+
+// DO WHILE
+var c = 1
+do  {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 5)
+
+    // WHILE
+var d = 1
+while (d <= 5) {
+    console.log(`passo ${d}`)    
+    d++
+}    
