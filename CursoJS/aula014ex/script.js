92ba65ed27233ef205951381b81eaa7e240b0d97
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2024-08-28 19:44:31 -0300

    comit

--- CursoJS/aula014ex/script.js
+++ CursoJS/aula014ex/script.js
@@ -1,3 +1,20 @@
-function gerarTaboada() {
-   let inicio = document.querySelector('inicio') 
+function tabuada() {
+   let num = document.getElementById('txtn') 
+   let tab = document.getElementById('seltab')
+
+   if(num.value.length == 0){
+      window.alert('Por favor digitar um numero.')
+   } else {
+      let n = Number(num.value)
+      let c = 1
+
+    tab.innerHTML = ''
+      while(c <= 10) {
+      let item = document.createElement('option')
+      item.text = `${n} x ${c} = ${n * c}`
+      item.value =`tab${c}`
+      tab.appendChild(item)
+      c++
+      }
+   }
 }
